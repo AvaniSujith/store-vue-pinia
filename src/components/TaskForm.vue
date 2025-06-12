1a8aa4784cf86dfe2ac47f4fc08a751c8dc4cd99
AvaniSujith <chinchuavani123@gmail.com>
2025-06-12 09:28:18 +0530

    bug fixing on the store functionality

--- src/components/TaskForm.vue
+++ src/components/TaskForm.vue
@@ -1,33 +1,51 @@
 <script setup>
-import { useTaskStore } from '../stores/TaskStore';
+// import { useTaskStore } from '../stores/TaskStore';
 
-import { ref } from 'vue';
+// import { reactive } from 'vue';
+
+// const taskStore = useTaskStore();
+
+// const inputData = reactive( { newTask: '' })
+
+// const handleSubmit = (inputData) => {
+//     if(inputData.newTask.value.length > 0){
+//         taskStore.addTask({
+//             title: inputData.newTask,
+//             isFav: false,
+//             id: Math.floor(Math.random() * 10000)
+//         })
+//         inputData.newTask= ''
+//     }
+// }
+
+import { useTaskStore } from "../stores/TaskStore";
+
+import { ref } from "vue";
 
 const taskStore = useTaskStore();
 
-const newTask = ref('');
+// taskStore.addTask();
+
+const newTask = ref("");
 
 const handleSubmit = () => {
-    if(newTask.value.length > 0){
-        taskStore.addTask({
-            title: newTask.value,
-            isFav: false,
-            id: Math.floor(Math.random() * 10000)
-        })
-        newTask.value = ''
-    }
-}
-
+  if (newTask.value.length > 0) {
+    console.log(taskStore);
+    taskStore.addTask({
+      title: newTask.value,
+      isFav: false,
+      id: Math.floor(Math.random() * 10000),
+    });
+    newTask.value = "";
+  }
+};
 </script>
 
 <template>
-    <form @submit.prevent="taskStore.handleSubmit()">
-        <input 
-            type="text"
-            placeholder="I need to.."
-            v-model="newTask"
-        />
+  <form @submit.prevent="handleSubmit()">
+    <input type="text" placeholder="I need to.." v-model="newTask" />
 
-        <button type="submit">Add</button>
-    </form>
+    <button >Add</button>
+  </form>
+
 </template>
